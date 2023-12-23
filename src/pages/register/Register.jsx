3856520b10e07eccbb33b47d701cf456d0d06f96
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-12-23 21:32:32 +0530

    Regsiter user logic design.

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,37 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
 import "./register.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+
 const Register = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const [messsage, setMessage] = useState(null);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    if (formData.password != formData.confirmPassword) {
+      setMessage({
+        text: "*Password and Confirm password do not match",
+        type: "error",
+      });
+    }
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/register/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+
+      console.log(data);
+
+      setMessage({
+        text: data.success ? "*User registered successfully" : data.error,
+        type: data.success ? "success" : "error",
+      });
+
+      if (data.success) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      setMessage({
+        text: "*An error occur.Try again!",
+        type: "error",
+      });
+    }
+  };
+
   return (
     <div className="registerContainer">
       <div className="registerFormWrapper">
         <h2 className="header">Secret Diary</h2>
         <p className="paragaph-one">Keep your thoughts freely and securely!</p>
         <p className="paragaph-one">Interested? Sign In!</p>
-        <form>
+        <form onSubmit={handleRegister}>
           <input
             type="text"
             name="username"
+            value={formData.username}
             id="username"
             placeholder="Your Username"
+            onChange={handleChange}
           />
           <input
             type="email"
             name="email"
+            value={formData.email}
             id="email"
             placeholder="Your Email address"
+            onChange={handleChange}
           />
           <input
             type="password"
-            name="password1"
-            id="passowrd1"
+            name="password"
+            value={formData.password}
+            id="passowrd"
             placeholder="Your Password"
+            onChange={handleChange}
           />
           <input
             type="password"
-            name="password2"
-            id="password2"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            id="confirmPassword"
             placeholder="Confirm your password"
+            onChange={handleChange}
           />
           <button type="submit">Register</button>
         </form>
