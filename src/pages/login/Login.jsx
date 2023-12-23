394e114694c54dbc5c30ab6cdf49f26f97f45c1f
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-12-23 22:46:09 +0530

    login logic design

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,25 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import "./login.css";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 const Login = () => {
+  const [formData, setformData] = useState({
+    username: "",
+    password: "",
+  });
+  const [message, setMessage] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setformData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://127.0.0.1:8000/api/login/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+
+      console.log(data);
+
+      setMessage({
+        text: data.success ? "*You are logged in" : data.error,
+        type: data.succes ? "success" : "error",
+      });
+
+      if (data.success) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+      setMessage({
+        text: "*An error occur. Try again!",
+        type: "error",
+      });
+    }
+  };
+
   return (
     <div className="loginContainer">
       <div className="loginFormWrapper">
         <h2 className="header">Secret Diary</h2>
         <p className="paragaph-one">Keep your thoughts freely and securely!</p>
         <p className="paragaph-one">Welcome back</p>
-        <form>
+        <form onSubmit={handleLogin}>
           <input
             type="text"
             name="username"
+            value={formData.username}
             id="username"
             placeholder="Your Username"
+            onChange={handleChange}
           />
           <input
             type="password"
-            name="password1"
-            id="passowrd1"
+            name="password"
+            value={formData.password}
+            id="passowrd"
             placeholder="Your Password"
+            onChange={handleChange}
           />
 
           <button type="submit">Login</button>
