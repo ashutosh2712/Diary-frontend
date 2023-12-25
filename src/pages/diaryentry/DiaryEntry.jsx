8e32412b3bbae79dd09f1be4962672c254bea9ef
Ashutosh Kumar Roy <ashu00619@gmail.com>
2023-12-25 19:48:04 +0530

    Update Diary entry done.

--- src/pages/diaryentry/DiaryEntry.jsx
+++ src/pages/diaryentry/DiaryEntry.jsx
@@ -22,17 +22,40 @@ const DiaryEntry = () => {
     setDiaryEntry(data);
   };
 
+  const updateDiaryEntry = async () => {
+    try {
+      fetch(`http://127.0.0.1:8000/api/entries/${entryId}/update`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(diaryEntry),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleSubmit = () => {
+    updateDiaryEntry();
+    navigate("/");
+  };
+
   return (
     <div className="diaryEntryContainer">
       <div className="diaryEntryWrapper">
         <div className="diaryEntryHeader">
-          <img src={LeftArrow} className="backEntry" />
+          <img src={LeftArrow} className="backEntry" onClick={handleSubmit} />
           <div className="doneEntry">Done</div>
         </div>
         <div className="diaryBody">
           <textarea
+            onChange={(e) => {
+              setDiaryEntry({ ...diaryEntry, body: e.target.value });
+            }}
             name="diary"
             id="diary"
+            value={diaryEntry?.body}
             className="diaryText"
             rows="31"
           ></textarea>
